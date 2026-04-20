840c810bb44a4145c84b562f12539720c1f06eb3
Arnav <karnav292@gmail.com>
2026-04-20 14:56:40 +0530

    implemented kafka like systm in bk.py

--- bk.py
+++ bk.py
@@ -1,31 +1,31 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column,Integer,String
-from sqlalchemy.orm import declarative_base
+# implementing double ended queue system
+import json
+import time
+import threading
+socket=""
+def rec():
+    m=socket.recv(1024).decode("ascii")
+    d=json.loads(m)
+    return d
+def sen(d):
+    l=json.dumps(d)
+    socket.sendall(l.encode("UTF-8"))
+    return "Dn"
+L=[]
+def bt():
+    while True:
+        time.sleep(5)
+        data = {"name": "Alice", "age": 30, "city": "New York"}
+        for _ in range(10):L.append(json.dumps(data))
+        print(L)
+def bt1():
+    while True:
+        time.sleep(3)
+        while len(L)!=0:
+            L.pop()
+        print(L)
 
-engine = create_engine("mysql+pymysql://root:system@localhost/chatroom")
-
-Session = sessionmaker(bind=engine)
-session = Session()
-
-Base = declarative_base()
-
-class User(Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True)
-    usna = Column(String(50), unique=True)
-    pas = Column(String(255))
-    sec = Column(String(255))
-
-
-user = User(
-    usna="leo",
-    pas="hash123",
-    sec="key123"
-)
-
-session.add(user)
-session.commit()
-
-print("Dn")
+th=threading.Thread(target=bt,args=())
+th.start()
+t=threading.Thread(target=bt1,args=())
+t.start()
